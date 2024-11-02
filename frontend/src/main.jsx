b6910e7e51daadc84298310a306506a00aee10d9
Ekta <yoyoekta@gmail.com>
2024-11-02 18:03:15 +0530

    Setup frontend routes and tailwind config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    
+    <Route path="" element={<App />}>
+    </Route> 
+  )
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
